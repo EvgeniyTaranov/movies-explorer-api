1d5b73b4cdf1abc7aa06fda1a008a09595cbd9be
Evgeniy Taranov <je.taranov@yandex.ru>
2023-09-27 00:17:23 +0300

    Обновил доменное имя

--- app.js
+++ app.js
@@ -14,10 +14,10 @@ const { PORT, DATABASE_URL } = process.env;
 const corsOptions = {
   origin: ['http://localhost:3000',
     'http://localhost:4000',
-    'https://evgeniytaranovdiploma.nomoredomainsicu.ru',
-    'http://evgeniytaranovdiploma.nomoredomainsicu.ru',
-    'http://api.evgeniytaranovdiploma.nomoredomainsicu.ru',
-    'https://api.evgeniytaranovdiploma.nomoredomainsicu.ru'],
+    'https://evgeniytaranovdiploma.nomoredomainsrocks.ru;',
+    'http://evgeniytaranovdiploma.nomoredomainsrocks.ru;',
+    'http://api.evgeniytaranovdiploma.nnomoredomainsrocks.ru;',
+    'https://api.evgeniytaranovdiploma.nomoredomainsrocks.ru;'],
   credentials: true,
 };
 
